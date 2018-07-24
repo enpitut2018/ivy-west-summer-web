269ff5513e316203da1f8d31b9542d7ca8e4edde
guri3 <aloesquash.lock@gmail.com>
2018-07-24 16:20:37 +0900

    不動産情報を登録可能にした

--- app/controllers/estates_controller.rb
+++ app/controllers/estates_controller.rb
@@ -1,2 +1,25 @@
 class EstatesController < ApplicationController
+  def index
+  end
+
+  def new
+    @estate = Estate.new
+  end
+
+  def create
+    estate = Estate.new(estate_params)
+    estate.save
+  end
+
+  def all
+    @estates = Estate.all
+
+    render 'all', formats: 'json', handlers: 'jbuilder'
+  end
+
+  private
+
+  def estate_params
+    params.require(:estate).permit(:name, :longitude, :latitude, :price, :address)
+  end
 end
