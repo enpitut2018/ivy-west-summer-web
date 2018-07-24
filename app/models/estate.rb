6db00ed276600a4aa65882ada2c86cd274df0352
guri3 <aloesquash.lock@gmail.com>
2018-07-24 13:38:10 +0900

    annotate実施

--- app/models/estate.rb
+++ app/models/estate.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: estates
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  longitude  :decimal(9, 6)
+#  latitude   :decimal(9, 6)
+#  price      :integer
+#  address    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Estate < ApplicationRecord
   validates :name, presence: true
 end
